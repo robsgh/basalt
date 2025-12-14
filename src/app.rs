7ac65a4c2680ba2787a78c72ef52a3e00e1b3ed9
Rob Schmidt <rob@schminfra.net>
2025-12-14 15:20:23 -0800

    updated tests

--- src/app.rs
+++ src/app.rs
@@ -3,6 +3,8 @@ use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::symbols::border;
 use ratatui::widgets::Block;
 use ratatui::widgets::List;
+use ratatui::widgets::ListItem;
+use ratatui::widgets::ListState;
 use ratatui::widgets::Padding;
 use ratatui::{DefaultTerminal, prelude::*};
 
@@ -15,12 +17,21 @@ pub enum BasaltState {
 }
 
 #[derive(Debug, Default, Clone)]
+struct BasaltData {
+    files: Vec<&'static str>,
+    list_state: ListState,
+}
+
+#[derive(Debug, Default, Clone)]
 pub struct BasaltApp {
     state: BasaltState,
+    data: BasaltData,
 }
 
 impl BasaltApp {
     pub fn run(&mut self) -> Result<()> {
+        self.data.files = vec!["file 1", "file 2", "file 3"];
+
         let mut terminal = ratatui::init();
         let res = self.ui_loop(&mut terminal);
         ratatui::restore();
@@ -45,18 +56,11 @@ impl BasaltApp {
             .padding(Padding::uniform(1))
             .border_set(border::THICK);
 
-        let files = List::new(
-            // self.files
-            //     .iter()
-            //     .map(|s| ListItem::new(*s))
-            //     .collect::<Vec<ListItem>>(),
-            ["file 1", "file 2", "file 3"],
-        )
-        .block(block)
-        .highlight_style(Style::new().bold().black().on_white());
+        let files = List::new(self.data.files.iter().map(|s| ListItem::from(*s)))
+            .block(block)
+            .highlight_style(Style::new().bold().black().on_white());
 
-        // frame.render_stateful_widget(files, frame.area(), &mut self.file_list_state);
-        frame.render_widget(files, frame.area());
+        frame.render_stateful_widget(files, frame.area(), &mut self.data.list_state);
     }
 
     fn handle_events(&mut self) -> Result<()> {
@@ -75,20 +79,20 @@ impl BasaltApp {
             KeyCode::Char('q') | KeyCode::Esc => {
                 self.state = BasaltState::Exiting;
             }
-            // KeyCode::Char('j') | KeyCode::Down => {
-            //     if self.file_list_state.selected().is_none() {
-            //         self.file_list_state.select_first();
-            //     } else {
-            //         self.file_list_state.select_next();
-            //     }
-            // }
-            // KeyCode::Char('k') | KeyCode::Up => {
-            //     if self.file_list_state.selected().is_none() {
-            //         self.file_list_state.select_last();
-            //     } else {
-            //         self.file_list_state.select_previous();
-            //     }
-            // }
+            KeyCode::Char('j') | KeyCode::Down => {
+                if self.data.list_state.selected().is_none() {
+                    self.data.list_state.select_first();
+                } else {
+                    self.data.list_state.select_next();
+                }
+            }
+            KeyCode::Char('k') | KeyCode::Up => {
+                if self.data.list_state.selected().is_none() {
+                    self.data.list_state.select_last();
+                } else {
+                    self.data.list_state.select_previous();
+                }
+            }
             _ => {}
         }
     }
@@ -99,37 +103,39 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_render() {
-        // let interface = BasaltInterface::default();
-        // let mut buf = Buffer::empty(Rect::new(0, 0, 50, 4));
-
-        // interface.render(buf.area, &mut buf);
-
-        // let title_style = Style::new().bold();
-        // let counter_style = Style::new().yellow();
-        // let key_style = Style::new().blue().bold();
-
-        // let mut snapshot = Buffer::with_lines(vec![
-        //     "┏━━━━━━━━━━━━━ Counter App Tutorial ━━━━━━━━━━━━━┓",
-        //     "┃                    Value: 0                    ┃",
-        //     "┃                                                ┃",
-        //     "┗━ Decrement <Left> Increment <Right> Quit <Q> ━━┛",
-        // ]);
-        // snapshot.set_style(Rect::new(14, 0, 22, 1), title_style);
-        // snapshot.set_style(Rect::new(28, 1, 1, 1), counter_style);
-        // snapshot.set_style(Rect::new(13, 3, 6, 1), key_style);
-        // snapshot.set_style(Rect::new(30, 3, 7, 1), key_style);
-        // snapshot.set_style(Rect::new(43, 3, 4, 1), key_style);
-
-        // assert_eq!(buf, snapshot);
-    }
-
-    #[test]
     fn test_handle_key_event() -> Result<()> {
         let mut app = BasaltApp::default();
 
         app.handle_key_event(KeyCode::Char('q').into());
         assert_eq!(app.state, BasaltState::Exiting);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_file_list_nav() -> Result<()> {
+        let mut app = BasaltApp::default();
+        app.data.files = vec!["1", "2", "3"];
+
+        assert_eq!(app.data.list_state.selected(), None);
+
+        // up down nav
+        app.handle_key_event(KeyCode::Char('j').into());
+        assert_eq!(app.data.list_state.selected(), Some(0));
+        app.handle_key_event(KeyCode::Char('j').into());
+        assert_eq!(app.data.list_state.selected(), Some(1));
+        app.handle_key_event(KeyCode::Char('k').into());
+        assert_eq!(app.data.list_state.selected(), Some(0));
+
+        // cannot go up beyond top
+        app.handle_key_event(KeyCode::Char('k').into());
+        assert_eq!(app.data.list_state.selected(), Some(0));
+
+        // cannot go down beyond bottom
+        app.data.list_state.select_last();
+        let last_idx = app.data.list_state.selected();
+        app.handle_key_event(KeyCode::Char('j').into());
+        assert_eq!(app.data.list_state.selected(), last_idx);
 
         Ok(())
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,5 @@
+///! Basalt
+///!
+///! A terminal UI based note taking application that prioritizes quickly creating notes without
+///! compromising on functionality.
 pub mod app;
