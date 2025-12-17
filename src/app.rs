1e3fae7ff245e3913d2c6907bf3d4ed3d7ac55b9
Rob Schmidt <rob+git@schminfra.net>
2025-12-16 21:44:01 -0800

    split up bundle code into separate module

--- src/app.rs
+++ src/app.rs
@@ -1,22 +1,21 @@
 use std::fs;
-use std::path::Path;
-use std::path::PathBuf;
 
 use anyhow::Context;
 use anyhow::Result;
 use anyhow::bail;
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use directories::ProjectDirs;
-use log::debug;
-use log::error;
+use directories::UserDirs;
 use log::info;
 use ratatui::widgets::Block;
-use ratatui::widgets::Borders;
 use ratatui::widgets::List;
 use ratatui::widgets::ListItem;
 use ratatui::widgets::ListState;
 use ratatui::widgets::Paragraph;
 use ratatui::{DefaultTerminal, prelude::*};
+
+use crate::bundles::Bundle;
+use crate::bundles::BundleLoader;
 
 /// State of the Basalt application
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd)]
@@ -30,46 +29,26 @@ pub enum BasaltState {
 /// Data model used by the Basalt application
 #[derive(Debug, Default, Clone)]
 struct BasaltData {
-    files: Vec<PathBuf>,
+    bundle: Bundle,
     list_state: ListState,
-    current_file: Option<PathBuf>,
-    current_file_contents: Option<String>,
 }
 
 /// The main Basalt Application
 #[derive(Debug, Default, Clone)]
 pub struct BasaltApp {
+    /// Running state of Basalt
     state: BasaltState,
+    /// Data used by Basalt to store bundle and stateful contents
     data: BasaltData,
-    current_dir: PathBuf,
 }
 
 impl BasaltApp {
-    /// Create a new directory for the Basalt project data
-    fn create_new_dir(&self, dir: &PathBuf) -> Result<()> {
-        info!(
-            "Creating basalt data directory at {}",
-            dir.to_string_lossy()
-        );
-        fs::create_dir_all(dir).with_context(|| {
-            format!(
-                "failed to create basalt data directory at {}",
-                dir.to_string_lossy()
-            )
-        })?;
-
-        Ok(())
-    }
-
-    /// Load `self.files` from a directory
-    fn load_from_dir(&self, dir: &PathBuf) -> Result<Vec<PathBuf>> {
-        info!("Basalt data directory exists, reusing it");
-
-        let files = fs::read_dir(dir)?
-            .filter_map(|d| d.ok().map(|entry| entry.path()))
-            .collect();
-
-        Ok(files)
+    /// Get the project-only dirs for the application
+    ///
+    /// This is for Basalt config, state, and application data
+    fn project_dirs() -> Result<ProjectDirs> {
+        ProjectDirs::from("com", "Schminfra", "Basalt")
+            .context("failed to create project directory for basalt")
     }
 
     /// Begin the application
@@ -80,20 +59,19 @@ impl BasaltApp {
     /// change to [`BasaltState::Exiting`] and the program will restore the terminal session.
     pub fn run(&mut self) -> Result<()> {
         if self.state == BasaltState::Init {
-            let dirs = ProjectDirs::from("com", "Schminfra", "Basalt")
-                .context("failed to create project directory for basalt")?;
-            self.current_dir = dirs.data_dir().to_path_buf();
+            if let Some(dirs) = UserDirs::new()
+                && let Some(docs) = dirs.document_dir()
+            {
+                let default_bundle = docs.join("init");
 
-            // TODO: Instead of just creating a dir and putting files in it, we should group dirs
-            // and consider each subdir as a single top-level note bundle
-            match fs::exists(&self.current_dir) {
-                Ok(false) => self.create_new_dir(&self.current_dir)?,
-                Ok(true) => {
-                    let files = self.load_from_dir(&self.current_dir)?;
-                    self.data.files = files;
+                if fs::exists(&default_bundle).context("failed to check dir existence of Bundle")? {
+                    self.data.bundle = BundleLoader::new(&default_bundle).load()?;
+                } else {
+                    self.data.bundle = BundleLoader::new(&default_bundle).init()?;
                 }
-                Err(e) => bail!("failed to stat basalt data dir: {:?}", e),
-            };
+            } else {
+                bail!("Failed to load ~/Documents directory from UserDirs")
+            }
         }
 
         info!("Entering running state");
@@ -117,16 +95,6 @@ impl BasaltApp {
         Ok(())
     }
 
-    /// Read the contents of a file and return a string that contains it all
-    ///
-    /// This method returns a string with an error message if reading the file failed.
-    fn read_file_contents(&self, path: &Path) -> String {
-        match fs::read_to_string(path) {
-            Ok(f) => f,
-            Err(e) => format!("Error reading file: {e:?}"),
-        }
-    }
-
     /// Organize the immediate mode TUI layout and present it to the screen
     fn draw(&mut self, frame: &mut Frame) {
         let [left_chunk, main_chunk] =
@@ -148,29 +116,14 @@ impl BasaltApp {
             .title_alignment(Alignment::Center)
             .title("Basalt");
 
-        let files = List::new(
-            self.data
-                .files
-                .iter()
-                .filter_map(|pb| {
-                    pb.strip_prefix(&self.current_dir)
-                        .ok()
-                        .map(|p| p.to_string_lossy())
-                })
-                .map(|f| ListItem::from(f)),
-        )
-        .block(nav_block)
-        .highlight_style(Style::new().bold().black().on_white());
-        frame.render_stateful_widget(files, nav_chunk, &mut self.data.list_state);
+        let notes = List::new(self.data.bundle.get_note_names())
+            .block(nav_block)
+            .highlight_style(Style::new().bold().black().on_white());
+        frame.render_stateful_widget(notes, nav_chunk, &mut self.data.list_state);
 
-        let main_contents = match &self.data.current_file {
+        let main_contents = match &self.data.list_state.selected() {
             None => "No file selected.",
-            Some(file_path) => {
-                if self.data.current_file_contents.is_none() {
-                    self.data.current_file_contents = Some(self.read_file_contents(&file_path));
-                }
-                self.data.current_file_contents.as_deref().unwrap()
-            }
+            Some(idx) => self.data.bundle.get(*idx).expect("Note exists"),
         };
         let main = Paragraph::new(main_contents).centered().block(main_block);
         frame.render_widget(main, main_chunk);
@@ -187,35 +140,12 @@ impl BasaltApp {
     fn handle_events(&mut self) -> Result<()> {
         match event::read()? {
             Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                self.handle_key_event(key_event)
+                self.handle_key_event(key_event)?
             }
             _ => {}
         }
 
         Ok(())
-    }
-
-    /// Refresh the list of files shown in the TUI
-    fn refresh_file_list(&mut self) {
-        match self.load_from_dir(&self.current_dir) {
-            Ok(files) => {
-                debug!("refreshed file list: {:?}", files);
-                self.data.files = files;
-            }
-            Err(e) => {
-                error!(
-                    "failed to load files in directory {:?}: {:?}",
-                    self.current_dir.to_string_lossy(),
-                    e
-                );
-            }
-        };
-    }
-
-    /// Set the current file in the data model and invalidate the stored current file contents
-    fn set_current_file(&mut self, path_buf: PathBuf) {
-        self.data.current_file = Some(path_buf);
-        self.data.current_file_contents = None;
     }
 
     /// Select the next file in the file list and update the data model's contents to ensure that
@@ -225,15 +155,6 @@ impl BasaltApp {
             self.data.list_state.select_first();
         } else {
             self.data.list_state.select_next();
-        }
-
-        if let Some(idx) = self.data.list_state.selected() {
-            let path_buf = self.data.files.get(idx).cloned();
-            if path_buf.is_none() {
-                return;
-            }
-
-            self.set_current_file(path_buf.unwrap());
         }
     }
 
@@ -245,15 +166,10 @@ impl BasaltApp {
         } else {
             self.data.list_state.select_previous();
         }
-
-        if let Some(idx) = self.data.list_state.selected() {
-            self.data.current_file = Some(self.data.files[idx].clone());
-            self.data.current_file_contents = None;
-        }
     }
 
     /// Handle keyboard events from the terminal
-    fn handle_key_event(&mut self, key_event: KeyEvent) {
+    fn handle_key_event(&mut self, key_event: KeyEvent) -> Result<()> {
         match key_event.code {
             KeyCode::Char('q') | KeyCode::Esc => {
                 self.state = BasaltState::Exiting;
@@ -265,10 +181,12 @@ impl BasaltApp {
                 self.select_prev_file();
             }
             KeyCode::Char('r') => {
-                self.refresh_file_list();
+                self.data.bundle = BundleLoader::new(self.data.bundle.get_path()).load()?;
             }
             _ => {}
         }
+
+        Ok(())
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,4 @@
 ///! A terminal UI based note taking application that prioritizes quickly creating notes without
 ///! compromising on functionality.
 pub mod app;
+mod bundles;
